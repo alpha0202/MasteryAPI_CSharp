84824621bfac55323c4c20be73a6b316d94cfba4
edwin martinez <alpha0202@gmail.com>
2023-05-31 16:35:19 -0500

    creando interfaz y clases

---
+++ Api.FurnitureStore.Share/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.FurnitureStore.Share
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int OrderNumber { get; set; }
+        public int ClientId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
+    }
+}
---
+++ Api.FurnitureStore.Share/ProductCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.FurnitureStore.Share
+{
+    public class ProductCategory
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
