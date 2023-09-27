ee7f245cccc9d1df07d542a9ce177bd677ffb81f
edwin harley martinez gomez <alpha0202@gmail.com>
2023-09-26 23:02:11 -0500

    Creación de los controladores

---
+++ Api.FurnitureStore.API/Controllers/ClientsController.cs
@@ -0,0 +1,71 @@
+﻿using Api.FurnitureStore.Data;
+using Api.FurnitureStore.Share;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.FurnitureStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly APIFunitureStoreContext _context;
+
+        public ClientsController(APIFunitureStoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Client>> Get()
+        {
+            return await _context.Clients.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDetails(int id)
+        {
+            var client = await _context.Clients.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(client == null)
+            {
+                return NotFound();
+
+            }
+            return Ok(client);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Client client)
+        {
+            await _context.Clients.AddAsync(client);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("post", client.Id, client);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(Client client)
+        {
+            _context.Clients.Update(client);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Client client)
+        {
+            if(client == null)
+            {
+                return NotFound(); 
+            }
+            _context.Clients.Remove(client);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Api.FurnitureStore.API/Controllers/ProductCategoryController.cs
@@ -0,0 +1,30 @@
+﻿using Api.FurnitureStore.Data;
+using Api.FurnitureStore.Share;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.FurnitureStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductCategoryController : ControllerBase
+    {
+        private readonly APIFunitureStoreContext _context;
+
+
+        public ProductCategoryController(APIFunitureStoreContext context)
+        {
+             _context = context;
+        }
+
+
+        [HttpGet]
+         public async Task<IEnumerable<ProductCategory>> Get()
+        {
+            return await _context.ProductCategories.ToListAsync();
+        }
+
+
+    }
+}
